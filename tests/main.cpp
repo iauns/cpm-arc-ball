93072f30045131a8169f153b1f1b0386d6c3135d
James Hughes <jhughes@sci.utah.edu>
2013-11-22 15:20:08 -0700

    Adding in google test code.

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,11 +1,13 @@
 
 #include <cstdlib>
 #include <iostream>
+#include "gtest/gtest.h"
 
 #include "gl-batch-context/Context.hpp"
 
-int main(int /*argc*/, char** /*argv*/)
+int main(int argc, char** argv)
 {
-
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
 
