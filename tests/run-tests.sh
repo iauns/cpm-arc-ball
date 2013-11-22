5dfaaa9b700ca3d94bab44e3d13dd323818403f5
James Hughes <jhughes@sci.utah.edu>
2013-11-22 10:03:25 -0700

    Adding arcball tests.

---
+++ tests/run-tests.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+
+if [ ! -d ./bin ]; then
+  mkdir -p ./bin
+fi
+
+# Ensure we fail immediately if any command fails.
+set -e
+
+pushd ./bin > /dev/null
+  cmake -DUSE_OS_MESA=ON ..
+  make
+popd
