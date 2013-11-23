cc10ac97c52c79b546e8420cccf4ffe7bd9a4035
James Hughes <jhughes@sci.utah.edu>
2013-11-23 08:44:32 -0700

    Small comment regarding PNG library that should be used.

--- tests/main.cpp
+++ tests/main.cpp
@@ -5,6 +5,8 @@
 
 #include "gl-batch-context/Context.hpp"
 
+/// \todo Use lodepng to write png files. It's small and we can easily
+///       create a CPM external for it.
 int main(int argc, char** argv)
 {
   ::testing::InitGoogleTest(&argc, argv);
