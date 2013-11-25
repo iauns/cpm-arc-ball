cfda32ec2959a7fb30d9d66b6f9e86be5080bb1b
James Hughes <jhughes@sci.utah.edu>
2013-11-25 15:33:21 -0700

    Fix spire test fixture.
    This test fixture should be moved to spire-testing.

--- tests/SpireTestFixture.cpp
+++ tests/SpireTestFixture.cpp
@@ -1,30 +1,3 @@
-/*
-   For more information, please see: http://software.sci.utah.edu
-
-   The MIT License
-
-   Copyright (c) 2013 Scientific Computing and Imaging Institute,
-   University of Utah.
-
-
-   Permission is hereby granted, free of charge, to any person obtaining a
-   copy of this software and associated documentation files (the "Software"),
-   to deal in the Software without restriction, including without limitation
-   the rights to use, copy, modify, merge, publish, distribute, sublicense,
-   and/or sell copies of the Software, and to permit persons to whom the
-   Software is furnished to do so, subject to the following conditions:
-
-   The above copyright notice and this permission notice shall be included
-   in all copies or substantial portions of the Software.
-
-   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-   OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-   THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-   DEALINGS IN THE SOFTWARE.
-*/
 
 /// \author James Hughes
 /// \date   November 2013
@@ -63,34 +36,36 @@ void SpireTestFixture::SetUp()
   shaderSearchDirs.push_back("shaders");
 
   // Build spire using the context from GlobalTestEnvironment.
-  std::shared_ptr<spire::Context> ctx = GlobalTestEnvironment::instance()->getContext();
+  std::shared_ptr<CPM_GL_BATCH_ENV_NS::BatchEnvironment> ctx 
+      = GlobalTestEnvironment::instance()->getBatchEnvironment();
+
+  mSpireContext = std::shared_ptr<SpireContext>(new SpireContext(ctx));
+
   ctx->makeCurrent();
-  mSpire = std::shared_ptr<spire::Interface>(new spire::Interface(
-          ctx, shaderSearchDirs));
+  mSpire = std::shared_ptr<CPM_SPIRE_NS::Interface>(
+      new CPM_SPIRE_NS::Interface(mSpireContext, shaderSearchDirs));
 
   // Add default attributes.
   addDefaultAttributes();
-
-  // Build camera that we will use for testing purposes.
-  mCamera = std::unique_ptr<TestCamera>(new TestCamera);
 }
 
 //------------------------------------------------------------------------------
 void SpireTestFixture::TearDown()
 {
   mSpire.reset();
+  mSpireContext.reset();
 }
 
 //------------------------------------------------------------------------------
 void SpireTestFixture::addDefaultAttributes()
 {
-  mSpire->addShaderAttribute("aPos",         3,  false,  sizeof(float) * 3,  spire::Interface::TYPE_FLOAT);
-  mSpire->addShaderAttribute("aNormal",      3,  false,  sizeof(float) * 3,  spire::Interface::TYPE_FLOAT);
-  mSpire->addShaderAttribute("aColorFloat",  4,  false,  sizeof(float) * 4,  spire::Interface::TYPE_FLOAT);
-  mSpire->addShaderAttribute("aColor",       4,  true,   sizeof(char) * 4,   spire::Interface::TYPE_UBYTE);
+  mSpire->addShaderAttribute("aPos",         3,  false,  sizeof(float) * 3,  CPM_SPIRE_NS::Interface::TYPE_FLOAT);
+  mSpire->addShaderAttribute("aNormal",      3,  false,  sizeof(float) * 3,  CPM_SPIRE_NS::Interface::TYPE_FLOAT);
+  mSpire->addShaderAttribute("aColorFloat",  4,  false,  sizeof(float) * 4,  CPM_SPIRE_NS::Interface::TYPE_FLOAT);
+  mSpire->addShaderAttribute("aColor",       4,  true,   sizeof(char) * 4,   CPM_SPIRE_NS::Interface::TYPE_UBYTE);
 
   // Used to test attribute mis-alignment.
-  mSpire->addShaderAttribute("aFieldData",   1,  false,  sizeof(float) * 1,  spire::Interface::TYPE_FLOAT);
+  mSpire->addShaderAttribute("aFieldData",   1,  false,  sizeof(float) * 1,  CPM_SPIRE_NS::Interface::TYPE_FLOAT);
 }
 
 
--- tests/SpireTestFixture.hpp
+++ tests/SpireTestFixture.hpp
@@ -5,6 +5,12 @@
 #ifndef IAUNS_SPIREFIXTURE_HPP
 #define IAUNS_SPIREFIXTURE_HPP
 
+#include <gl-batch-env/BatchEnvironment.hpp>
+#include <spire/Interface.h>
+#include <gtest/gtest.h>
+
+#include "GlobalTestEnvironment.hpp"
+
 class SpireContext : public CPM_SPIRE_NS::Context
 {
 public:
@@ -20,49 +26,14 @@ private:
 class SpireTestFixture : public testing::Test
 {
 public:
-  InterfaceTestFixture() {}
+  SpireTestFixture() {}
 
-  virtual void SetUp() override
-  {
-    std::vector<std::string> shaderSearchDirs;
-    shaderSearchDirs.push_back("shaders");
+  virtual void SetUp() override;
+  virtual void TearDown() override;
 
-    // Build spire using the context from GlobalTestEnvironment.
-    std::shared_ptr<CPM_GL_BATCH_ENV_NS::BatchEnvironment> ctx 
-        = GlobalTestEnvironment::instance()->getBatchEnvironment();
-
-    mSpireContext = std::shared_ptr<SpireContext>(ctx);
-
-    ctx->makeCurrent();
-    mSpire = std::shared_ptr<CPM_SPIRE_NS::Interface>(
-        new CPM_SPIRE_NS::Interface(mSpireContext, shaderSearchDirs));
-
-    // Add default attributes.
-    addDefaultAttributes();
-
-    // Build camera that we will use for testing purposes.
-    mCamera = std::unique_ptr<TestCamera>(new TestCamera);
-  }
-
-  virtual void TearDown() override
-  {
-    mSpire.reset();
-    mSpireContext.reset();
-  }
-
-  void addDefaultAttributes()
-  {
-    mSpire->addShaderAttribute("aPos",         3,  false,  sizeof(float) * 3,  CPM_SPIRE_NS::Interface::TYPE_FLOAT);
-    mSpire->addShaderAttribute("aNormal",      3,  false,  sizeof(float) * 3,  CPM_SPIRE_NS::Interface::TYPE_FLOAT);
-    mSpire->addShaderAttribute("aColorFloat",  4,  false,  sizeof(float) * 4,  CPM_SPIRE_NS::Interface::TYPE_FLOAT);
-    mSpire->addShaderAttribute("aColor",       4,  true,   sizeof(char) * 4,   CPM_SPIRE_NS::Interface::TYPE_UBYTE);
-
-    // Used to test attribute mis-alignment.
-    mSpire->addShaderAttribute("aFieldData",   1,  false,  sizeof(float) * 1,  CPM_SPIRE_NS::Interface::TYPE_FLOAT);
-  }
+  void addDefaultAttributes();
 
   std::shared_ptr<CPM_SPIRE_NS::Interface>    mSpire;
-  std::unique_ptr<TestCamera>                 mCamera;
 
 private:
 
