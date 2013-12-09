86a1fd08426341aeaae268a0702bb0a962bdd1fa
James Hughes <jhughes@sci.utah.edu>
2013-12-09 13:04:33 -0700

    Remove license from top of source file.

--- arc-ball/ArcBall.hpp
+++ arc-ball/ArcBall.hpp
@@ -1,34 +1,3 @@
-/*
-   For more information, please see: http://software.sci.utah.edu
-
-   The MIT License
-
-   Copyright (c) 2013 Scientific Computing and Imaging Institute,
-   University of Utah.
-
-
-   Permission is hereby granted, free of charge, to any person obtaining a
-   copy of this software and associated documentation files (the "Software"),
-   to deal in the Software without restriction, including without limitation
-   the rights to use, copy, modify, merge, publish, distribute, sublicense,
-   and/or sell copies of the Software, and to permit persons to whom the
-   Software is furnished to do so, subject to the following conditions:
-
-   The above copyright notice and this permission notice shall be included
-   in all copies or substantial portions of the Software.
-
-   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-   OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-   THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-   DEALINGS IN THE SOFTWARE.
-*/
-
-/// \author James Hughes
-/// \date   April 2013
-
 #ifndef IAUNS_ARC_BALL_H
 #define IAUNS_ARC_BALL_H
 
@@ -63,7 +32,7 @@ public:
   /// \param radius         Radius in TCS. For screen coordinates, a good
   ///                       default is 0.75.
   /// \param screenToTCS    Transformation from screen coordinates
-  ///                       to TCS. 'center' and 'radius' are given in TCS.
+  ///                       to TCS. \p center and \p radius are given in TCS.
   ArcBall(const glm::vec3& center, float radius,
           const glm::mat4& screenToTCS = glm::mat4());
   virtual ~ArcBall();
